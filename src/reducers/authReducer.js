96b34e515713be988c193753f03c26d55304dedc
Rohit Pandit <rohit0311@outlook.com>
2021-05-15 09:28:55 +0530

    adds authReducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,49 @@
+import {
+	LOGIN_FAIL,
+	LOGIN_REQUEST,
+	LOGIN_SUCCESS,
+	SIGNUP_FAIL,
+	SIGNUP_REQUEST,
+	SIGNUP_SUCCESS,
+} from '../constants/authConstants';
+
+const initialState = {
+	error: null,
+	success: false,
+	token: null,
+	loading: false,
+};
+
+const authReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case LOGIN_REQUEST:
+		case SIGNUP_REQUEST:
+			return {
+				...state,
+				loading: true,
+				token: null,
+				error: null,
+				success: false,
+			};
+		case SIGNUP_SUCCESS:
+		case LOGIN_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				token: action.payload,
+				success: true,
+			};
+		case SIGNUP_FAIL:
+		case LOGIN_FAIL:
+			return {
+				...state,
+				error: action.error,
+				success: false,
+				loading: false,
+			};
+		default:
+			return state;
+	}
+};
+
+export default authReducer;
