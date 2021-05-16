caf50b4ec3742deb8cc97db726af32a252156b46
Rohit Pandit <rohit0311@outlook.com>
2021-05-16 15:31:56 +0530

    adds dummy product page

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
+import Footer from '../../components/footer/Footer';
 import classes from './Product.module.css';
+import shirt from '../home/shirt.jpg';
 
 const Product = () => {
 	return (
 		<div className={classes.page}>
 			<Navbar />
-			<div>Login page</div>
+			<div className={`${classes.main} container shadow`}>
+				<div className={classes.image}>
+					<img src={shirt} alt='' className={classes.imageImg} />
+				</div>
+				<div className={classes.info}>
+					<h3>Brand 1 shirt</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero
+						dolores autem. Dolorum optio velit hic amet officia tempora
+						nesciunt!
+					</p>
+					<h3 className='text-danger'>&#8377;1000.00</h3>
+					<button className='btn btn-outline-success'>
+						Add to Cart <i class='fas fa-shopping-cart'></i>
+					</button>
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 };
