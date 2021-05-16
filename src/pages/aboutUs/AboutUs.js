f8281090dd954d4fd93c895d0d263bab709430bc
Rohit Pandit <rohit0311@outlook.com>
2021-05-16 09:33:17 +0530

    adds contactus

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -8,20 +8,32 @@ const AboutUs = () => {
 	return (
 		<div className={classes.page}>
 			<Navbar />
-			<div className={`${classes.content} container`}>
-				<h1>Fashion at your fingertips</h1>
-				<h4>Thousands of happy customers served every month.</h4>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-					eaque totam, aperiam sapiente obcaecati perspiciatis ut laborum
-					tempora nesciunt nobis id cupiditate illo! Nulla dolorum voluptate
-					perspiciatis aspernatur aliquid quam. Lorem ipsum dolor, sit amet
-					consectetur adipisicing elit. Rerum maiores, deserunt facilis fuga
-					temporibus voluptates commodi aliquid earum consequuntur. Omnis!
-				</p>
-				<h5>
-					Got some queries? <Link to='/contact'>Contact Us</Link>
-				</h5>
+			<div className={`${classes.content} container shadow`}>
+				<h1>About Vastr</h1>
+				<hr />
+				<div className='px-5'>
+					<h2>Fashion at your fingertips</h2>
+					<h4>Thousands of happy customers served every month.</h4>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+						eaque totam, aperiam sapiente obcaecati perspiciatis ut laborum
+						tempora nesciunt nobis id cupiditate illo! Nulla dolorum voluptate
+						perspiciatis aspernatur aliquid quam. Lorem ipsum dolor, sit amet
+						consectetur adipisicing elit. Rerum maiores, deserunt facilis fuga
+						temporibus voluptates commodi aliquid earum consequuntur. Omnis!
+					</p>
+					<h5>
+						Got some queries? <Link to='/contact'>Contact Us</Link>
+					</h5>
+					<h6>
+						Check out the{' '}
+						<a
+							target='_blank'
+							href='https://github.com/rohitpandit0311/vastr-client'>
+							Code
+						</a>
+					</h6>
+				</div>
 			</div>
 			<Footer />
 		</div>
--- src/pages/contactUs/ContactUs.js
+++ src/pages/contactUs/ContactUs.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import Footer from '../../components/footer/Footer';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import classes from './ContactUs.module.css';
 
 const ContactUs = () => {
-	return <div>Contact us</div>;
+	const [email, setEmail] = useState('');
+	const [query, setQuery] = useState('');
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		setQuery('');
+		setEmail('');
+		toast.success('Query sent successfully');
+	};
+
+	return (
+		<div className={classes.page}>
+			<Navbar />
+			<ToastContainer />
+			<div className={`${classes.content} container shadow`}>
+				<h1>Contact us</h1>
+				<hr />
+				<form className='px-5' onSubmit={onSubmit}>
+					<input
+						type='email'
+						className='form-control my-2'
+						placeholder='Enter your email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+					<textarea
+						type='text'
+						className='form-control my-2'
+						rows='5'
+						placeholder='your query'
+						value={query}
+						onChange={(e) => setQuery(e.target.value)}
+						required
+					/>
+					<button className='btn  btn-outline-success m-3' type='submit'>
+						Submit
+					</button>
+				</form>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default ContactUs;
