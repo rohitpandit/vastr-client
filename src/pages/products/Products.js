c84480537288970ad16eb7aeaae306ac9497659e
Rohit Pandit <rohit0311@outlook.com>
2021-05-17 16:54:56 +0530

    adds addToOrder in products page

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
 import classes from './Products.module.css';
 import shirt from '../home/shirt.jpg';
 import pant from '../home/pant.jpg';
 import saree from '../home/saree.jpg';
+import { addToOrders } from '../../actions/orderAction';
+import { Link } from 'react-router-dom';
 
-const Products = () => {
+const Products = (props) => {
+	useEffect(() => {
+		if (props.orderSuccess) {
+			toast.success('Prduct added to cart');
+		}
+	}, [props.orderSuccess]);
+
+	useEffect(() => {
+		if (props.orderError) {
+			toast.error(props.orderError);
+		}
+	}, [props.orderError]);
+
+	const addToCart = (productId) => {
+		props.addToOrders(productId);
+	};
+
 	return (
 		<div className={classes.page}>
 			<Navbar />
 			<div className={`${classes.main} container`}>
+				<ToastContainer />
 				<div className={`${classes.filter} shadow`}>
 					<h3>Filter</h3>
 					<hr />
@@ -140,42 +162,62 @@ const Products = () => {
 				</div>
 				<div className={`${classes.content} `}>
 					<div className={`${classes.card} shadow`}>
-						<img src={saree} alt='' className='card-img-top' />
+						<Link to='/product'>
+							<img src={saree} alt='' className='card-img-top' />
+						</Link>
 						<h5 className='m-1'>Brand 1 Saree</h5>
 						<h5 className='text-danger'>&#8377;1000</h5>
-						<button className='btn btn-outline-success'>
+						<button
+							className='btn btn-outline-success'
+							onClick={(productId) => addToCart(productId)}>
 							Add to Cart <i class='fas fa-shopping-cart'></i>
 						</button>
 					</div>
 					<div className={`${classes.card} shadow`}>
-						<img src={shirt} alt='' className='card-img-top' />
+						<Link to='/product'>
+							<img src={shirt} alt='' className='card-img-top' />
+						</Link>
 						<h5 className='m-1'>Brand 1 Saree</h5>
 						<h5 className='text-danger'>&#8377;1000</h5>
-						<button className='btn btn-outline-success'>
+						<button
+							className='btn btn-outline-success'
+							onClick={(productId) => addToCart(productId)}>
 							Add to Cart <i class='fas fa-shopping-cart'></i>
 						</button>
 					</div>
 					<div className={`${classes.card} shadow`}>
-						<img src={pant} alt='' className='card-img-top' />
+						<Link to='/product'>
+							<img src={pant} alt='' className='card-img-top' />
+						</Link>
 						<h5 className='m-1'>Brand 1 Saree</h5>
 						<h5 className='text-danger'>&#8377;1000</h5>
-						<button className='btn btn-outline-success'>
+						<button
+							className='btn btn-outline-success'
+							onClick={(productId) => addToCart(productId)}>
 							Add to Cart <i class='fas fa-shopping-cart'></i>
 						</button>
 					</div>
 					<div className={`${classes.card} shadow `}>
-						<img src={saree} alt='' className='card-img-top' />
+						<Link to='/product'>
+							<img src={saree} alt='' className='card-img-top' />
+						</Link>
 						<h5 className='m-1'>Brand 1 Saree</h5>
 						<h5 className='text-danger'>&#8377;2000</h5>
-						<button className='btn btn-outline-success'>
+						<button
+							className='btn btn-outline-success'
+							onClick={(productId) => addToCart(productId)}>
 							Add to Cart <i class='fas fa-shopping-cart'></i>
 						</button>
 					</div>
 					<div className={`${classes.card} shadow`}>
-						<img src={saree} alt='' className='card-img-top' />
+						<Link to='/product'>
+							<img src={saree} alt='' className='card-img-top' />
+						</Link>
 						<h5 className='m-1'>Brand 1 Saree</h5>
 						<h5 className='text-danger'>&#8377;3000</h5>
-						<button className='btn btn-outline-success'>
+						<button
+							className='btn btn-outline-success'
+							onClick={(productId) => addToCart(productId)}>
 							Add to Cart <i class='fas fa-shopping-cart'></i>
 						</button>
 					</div>
@@ -186,4 +228,14 @@ const Products = () => {
 	);
 };
 
-export default Products;
+const mapStateToProps = (state) => ({
+	orderLoading: state.order.loading,
+	orderSuccess: state.order.success,
+	orderError: state.order.error,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	addToOrders: (productId) => dispatch(addToOrders(productId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
