39f3d20835f04fd2c682bd4a6991d17334c0b957
Rohit Pandit <rohit0311@outlook.com>
2021-07-19 11:51:59 +0530

    adds skeleton placeholder in product list pgae

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,74 +7,82 @@ import saree from './saree.jpg';
 import Footer from '../../components/footer/Footer';
 
 const Home = (props) => {
-	return (
-		<div className={classes.page}>
-			<Navbar />
-			<div className={classes.head}>
-				<h1 className='text-white shadow'>Clothes brings Happiness</h1>
-			</div>
-			<div className='container d-flex flex-column my-3'>
-				<div
-					className={`${classes.card} shadow`}
-					onClick={() => {
-						props.history.push('/products/pant');
-					}}>
-					<img src={pant} alt='pant' className='w-25' />
-					<div className='info'>
-						<h3 className='text-center'>Pants</h3>
-						<p className='text-center'>
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
-							a. Sapiente placeat dolorem rerum, accusamus culpa sint voluptatum
-							soluta. Similique praesentium reprehenderit fuga fugit asperiores
-							delectus possimus? Enim, sed dolor, excepturi architecto
-							blanditiis voluptates neque cupiditate eaque ducimus soluta
-							tenetur provident expedita consequatur veniam voluptatum earum
-							laboriosam quibusdam placeat minus.
-						</p>
-					</div>
-				</div>
-				<div
-					className={` ${classes.card} ${classes.shirt}  shadow`}
-					onClick={() => {
-						props.history.push('/products/shirt');
-					}}>
-					<img src={shirt} alt='shirt' className='w-25' />
-					<div className='info'>
-						<h3 className='text-center'>Shirt</h3>
-						<p className='text-center'>
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
-							a. Sapiente placeat dolorem rerum, accusamus culpa sint voluptatum
-							soluta. Similique praesentium reprehenderit fuga fugit asperiores
-							delectus possimus? Enim, sed dolor, excepturi architecto
-							blanditiis voluptates neque cupiditate eaque ducimus soluta
-							tenetur provident expedita consequatur veniam voluptatum earum
-							laboriosam quibusdam placeat minus.
-						</p>
-					</div>
-				</div>
-				<div
-					className={`${classes.card} shadow`}
-					onClick={() => {
-						props.history.push('/products/saree');
-					}}>
-					<img src={saree} alt='saree' className='w-25' />
-					<div className='info'>
-						<h3 className='text-center'>Saree</h3>
-						<p className='text-center'>
-							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
-							a. Sapiente placeat dolorem rerum, accusamus culpa sint voluptatum
-							soluta. Similique praesentium reprehenderit fuga fugit asperiores
-							delectus possimus? Enim, sed dolor, excepturi architecto
-							blanditiis voluptates neque cupiditate eaque ducimus soluta
-							tenetur provident expedita consequatur veniam voluptatum earum
-							laboriosam quibusdam placeat minus.
-						</p>
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+    return (
+        <div className={classes.page}>
+            <Navbar />
+            <div className={classes.head}>
+                <h1 className='text-white shadow'>Clothes brings Happiness</h1>
+            </div>
+            <div className='container d-flex flex-column my-3'>
+                <div
+                    className={`${classes.card} shadow`}
+                    onClick={() => {
+                        props.history.push('/products/pant');
+                    }}>
+                    <img src={pant} alt='pant' className='w-25' />
+                    <div className='info'>
+                        <h3 className='text-center'>Pants</h3>
+                        <p className='text-center'>
+                            There is some evidence, from figurative art, of
+                            trousers being worn in the Upper Paleolithic, as
+                            seen on the figurines found at the Siberian sites of
+                            Mal'ta and Buret'. The oldest known trousers were
+                            found at the Yanghai cemetery, extracted from
+                            mummies in Turpan, Xinjiang, western China,
+                            belonging to the people of the Tarim Basin; dated to
+                            the period between the 13th and the 10th century BC
+                            and made of wool, the trousers had straight legs and
+                            wide crotches, and were likely made for horseback
+                            riding.
+                        </p>
+                    </div>
+                </div>
+                <div
+                    className={` ${classes.card} ${classes.shirt}  shadow`}
+                    onClick={() => {
+                        props.history.push('/products/shirt');
+                    }}>
+                    <img src={shirt} alt='shirt' className='w-25' />
+                    <div className='info'>
+                        <h3 className='text-center'>Shirt</h3>
+                        <p className='text-center'>
+                            The world's oldest preserved garment, discovered by
+                            Flinders Petrie, is a "highly sophisticated" linen
+                            shirt from a First Dynasty Egyptian tomb at Tarkan,
+                            dated to c. 3000 BC: "the shoulders and sleeves have
+                            been finely pleated to give form-fitting trimness
+                            while allowing the wearer room to move. The small
+                            fringe formed during weaving along one edge of the
+                            cloth has been placed by the designer to decorate
+                            the neck opening and side seam."
+                        </p>
+                    </div>
+                </div>
+                <div
+                    className={`${classes.card} shadow`}
+                    onClick={() => {
+                        props.history.push('/products/saree');
+                    }}>
+                    <img src={saree} alt='saree' className='w-25' />
+                    <div className='info'>
+                        <h3 className='text-center'>Saree</h3>
+                        <p className='text-center'>
+                            History of Saree-like drapery is traced back to the
+                            Sindh region of the Indus Valley Civilisation, which
+                            flourished during 2800–1800 BCE around the
+                            northwestern part of the Indian subcontinent. Cotton
+                            was first cultivated and woven in Indian
+                            subcontinent around 5th millennium BCE. Dyes used
+                            during this period are still in use, particularly
+                            indigo, lac, red madder and turmeric. Silk was woven
+                            around 2450 BCE and 2000 BCE.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default Home;
--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
 import classes from './Products.module.css';
@@ -12,165 +14,194 @@ import { Link } from 'react-router-dom';
 import { getProductList } from '../../actions/productAction';
 
 const Products = (props) => {
-	const { type } = useParams();
-	const [update, setUpdate] = useState(false);
-	const [priceRange, setPriceRange] = useState(100000);
+    const { type } = useParams();
+    const [update, setUpdate] = useState(false);
+    const [priceRange, setPriceRange] = useState(100000);
 
-	useEffect(() => {
-		props.getProductList(type);
-		setPriceRange(100000);
-	}, [props.location]);
+    useEffect(() => {
+        props.getProductList(type);
+        setPriceRange(100000);
+    }, [props.location]);
 
-	useEffect(() => {
-		if (props.orderSuccess && update) {
-			toast.success('Prduct added to cart');
-			setUpdate(false);
-		}
-	}, [props.orderSuccess]);
+    useEffect(() => {
+        if (props.orderSuccess && update) {
+            toast.success('Prduct added to cart');
+            setUpdate(false);
+        }
+    }, [props.orderSuccess]);
 
-	useEffect(() => {
-		if (props.orderError) {
-			toast.error(props.orderError);
-		}
-	}, [props.orderError]);
+    useEffect(() => {
+        if (props.orderError) {
+            toast.error(props.orderError);
+        }
+    }, [props.orderError]);
 
-	const addToCart = (product) => {
-		if (!props.token) {
-			toast.error('You  must login first');
-			return;
-		}
-		setUpdate(true);
-		console.log(product);
-		props.addToOrders(product);
-	};
+    const addToCart = (product) => {
+        if (!props.token) {
+            toast.error('You  must login first');
+            return;
+        }
+        setUpdate(true);
+        console.log(product);
+        props.addToOrders(product);
+    };
 
-	return (
-		<div className={classes.page}>
-			<Navbar />
-			{(props.productLoading || props.orderLoading) && <Loading />}
-			<div className={`${classes.main} container`}>
-				<ToastContainer />
-				<div className={`${classes.filter} shadow`}>
-					<h3>Filter</h3>
-					<hr />
+    return (
+        <div className={classes.page}>
+            <Navbar />
+            {(props.productLoading || props.orderLoading) && <Loading />}
+            <div className={`${classes.main} container`}>
+                <ToastContainer />
+                <div className={`${classes.filter} shadow`}>
+                    <h3>Filter</h3>
+                    <hr />
 
-					<div className={classes.filterSection}>
-						<p>By Price</p>
+                    <div className={classes.filterSection}>
+                        <p>By Price</p>
 
-						<div className='form-check '>
-							<input
-								className='form-check-input'
-								type='radio'
-								name='inlineRadioOptions'
-								id='inlineRadio1'
-								value={1000}
-								onChange={(e) => setPriceRange(e.target.value)}
-							/>
-							<label className='form-check-label' htmlFor='inlineRadio1'>
-								Upto 1000
-							</label>
-						</div>
-						<div className='form-check '>
-							<input
-								className='form-check-input'
-								type='radio'
-								name='inlineRadioOptions'
-								id='inlineRadio1'
-								value={2000}
-								onChange={(e) => setPriceRange(e.target.value)}
-							/>
-							<label className='form-check-label' htmlFor='inlineRadio1'>
-								Upto 2000
-							</label>
-						</div>
-						<div className='form-check'>
-							<input
-								className='form-check-input'
-								type='radio'
-								name='inlineRadioOptions'
-								id='inlineRadio1'
-								value={3000}
-								onChange={(e) => setPriceRange(e.target.value)}
-							/>
-							<label className='form-check-label' htmlFor='inlineRadio1'>
-								Upto 3000
-							</label>
-						</div>
-						<div className='form-check '>
-							<input
-								className='form-check-input'
-								type='radio'
-								name='inlineRadioOptions'
-								id='inlineRadio1'
-								value={4000}
-								onChange={(e) => setPriceRange(e.target.value)}
-							/>
-							<label className='form-check-label' htmlFor='inlineRadio1'>
-								Upto 4000
-							</label>
-						</div>
-						<div className='form-check'>
-							<input
-								className='form-check-input'
-								type='radio'
-								name='inlineRadioOptions'
-								id='inlineRadio1'
-								value={5000}
-								onChange={(e) => setPriceRange(e.target.value)}
-							/>
-							<label className='form-check-label' htmlFor='inlineRadio1'>
-								Upto 5000
-							</label>
-						</div>
-					</div>
-				</div>
-				<div className={`${classes.content} `}>
-					{props.productList &&
-						props.productList.map((product) => {
-							if (product.price <= priceRange) {
-								return (
-									<div key={product._id} className={`${classes.card} shadow`}>
-										<div className={classes.image}>
-											<Link to={`/product/${product._id}`}>
-												<img
-													src={product.url}
-													alt=''
-													className={classes.cardImg}
-												/>
-											</Link>
-										</div>
-										<h5 className='m-1'>{product.desc}</h5>
-										<h5 className='text-danger'>&#8377;{product.price}</h5>
-										<button
-											className='btn btn-outline-success'
-											onClick={() => addToCart(product)}>
-											Add to Cart <i className='fas fa-shopping-cart'></i>
-										</button>
-									</div>
-								);
-							}
-						})}
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+                        <div className='form-check '>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='inlineRadioOptions'
+                                id='inlineRadio1'
+                                value={1000}
+                                onChange={(e) => setPriceRange(e.target.value)}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='inlineRadio1'>
+                                Upto 1000
+                            </label>
+                        </div>
+                        <div className='form-check '>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='inlineRadioOptions'
+                                id='inlineRadio1'
+                                value={2000}
+                                onChange={(e) => setPriceRange(e.target.value)}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='inlineRadio1'>
+                                Upto 2000
+                            </label>
+                        </div>
+                        <div className='form-check'>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='inlineRadioOptions'
+                                id='inlineRadio1'
+                                value={3000}
+                                onChange={(e) => setPriceRange(e.target.value)}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='inlineRadio1'>
+                                Upto 3000
+                            </label>
+                        </div>
+                        <div className='form-check '>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='inlineRadioOptions'
+                                id='inlineRadio1'
+                                value={4000}
+                                onChange={(e) => setPriceRange(e.target.value)}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='inlineRadio1'>
+                                Upto 4000
+                            </label>
+                        </div>
+                        <div className='form-check'>
+                            <input
+                                className='form-check-input'
+                                type='radio'
+                                name='inlineRadioOptions'
+                                id='inlineRadio1'
+                                value={5000}
+                                onChange={(e) => setPriceRange(e.target.value)}
+                            />
+                            <label
+                                className='form-check-label'
+                                htmlFor='inlineRadio1'>
+                                Upto 5000
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${classes.content} `}>
+                    {props.productList.length == 0
+                        ? [1, 2, 3].map((item) => (
+                              <section className={classes.card}>
+                                  <SkeletonTheme color='#fff'>
+                                      <Skeleton width={240} height={448} />
+                                  </SkeletonTheme>
+                              </section>
+                          ))
+                        : props.productList.map((product) => {
+                              if (product.price <= priceRange) {
+                                  return (
+                                      <div
+                                          key={product._id}
+                                          className={`${classes.card} shadow`}>
+                                          <div className={classes.image}>
+                                              <Link
+                                                  to={`/product/${product._id}`}>
+                                                  <img
+                                                      src={product.url}
+                                                      alt=''
+                                                      className={
+                                                          classes.cardImg
+                                                      }
+                                                  />
+                                              </Link>
+                                          </div>
+                                          <h5 className='m-1'>
+                                              {product.desc}
+                                          </h5>
+                                          <h5 className='text-danger'>
+                                              &#8377;{product.price}
+                                          </h5>
+                                          <button
+                                              className='btn btn-outline-success'
+                                              onClick={() =>
+                                                  addToCart(product)
+                                              }>
+                                              Add to Cart{' '}
+                                              <i className='fas fa-shopping-cart'></i>
+                                          </button>
+                                      </div>
+                                  );
+                              }
+                          })}
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => ({
-	token: state.auth.token,
-	orderLoading: state.order.loading,
-	orderSuccess: state.order.success,
-	orderError: state.order.error,
-	productLoading: state.product.loading,
-	productSuccess: state.product.success,
-	productError: state.product.error,
-	productList: state.product.products,
+    token: state.auth.token,
+    orderLoading: state.order.loading,
+    orderSuccess: state.order.success,
+    orderError: state.order.error,
+    productLoading: state.product.loading,
+    productSuccess: state.product.success,
+    productError: state.product.error,
+    productList: state.product.products,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	addToOrders: (product) => dispatch(addToOrders(product)),
-	getProductList: (type) => dispatch(getProductList(type)),
+    addToOrders: (product) => dispatch(addToOrders(product)),
+    getProductList: (type) => dispatch(getProductList(type)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
